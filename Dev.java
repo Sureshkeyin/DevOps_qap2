cf240e8bbfbef651642cdf70eacfb35b02372876
alexb454 <71828880+alexb454@users.noreply.github.com>
2021-10-27 10:50:25 -0230

    escape

--- Dev.java
+++ Dev.java
@@ -1,1 +1,11 @@
 //this is the uploaded file for this assignment
+public class Dev {
+    private String name;
+
+    public Dev(String name){
+        name = "Larry";
+    }
+    public String toString() {
+      return name;
+    }
+  }
